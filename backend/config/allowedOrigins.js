fd478eb6760deaf8ab671e172c5a0509d055f436
jabin <jabinv03@gmail.com>
2022-09-29 10:25:54 +0530

    deploy

--- backend/config/allowedOrigins.js
+++ backend/config/allowedOrigins.js
@@ -1,3 +1,3 @@
-const allowedOrigins=['https://www.mysite.com',"http://127.0.0.1:5500","http://localhost:3500","http://localhost:3001","http://localhost:3000","https://dry-fortress-44491.herokuapp.com"];
+const allowedOrigins=['https://www.mysite.comm',"http://127.0.0.1:5500","http://localhost:3500","http://localhost:3001","http://localhost:3000","https://dry-fortress-44491.herokuapp.com"];
 
 module.exports=allowedOrigins;
